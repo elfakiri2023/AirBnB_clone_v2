bf580e11b0acf4b0979b503e393413df978fd1f3
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-02-21 18:08:59 +0100

    Updated models/user.py

--- models/user.py
+++ models/user.py
@@ -11,9 +11,14 @@ class User(BaseModel, Base):
 
     __tablename__ = "users"
 
-    email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship('Place', backref="user",
-                          cascade='all, delete, delete-orphan')
+    first_name = Column(String(128))
+    email = Column(String(128), nullable=False)
+    last_name = Column(String(128))
+    places = relationship("Place",
+                          cascade='all, delete, delete-orphan',
+                          backref='user')
+    reviews = relationship("Review",
+                           cascade='all, delete, delete-orphan',
+                           backref='user')
+    
