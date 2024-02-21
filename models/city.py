df81168306812492798fffae22c169e74b4728ca
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-02-21 17:19:25 +0100

    Updated setup_mysql_test.sql

--- models/city.py
+++ models/city.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import Base
-from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
-
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'),
-                      nullable=False)
-    places = relationship('Place',
-                          backref='cities',
+    Places = relationship("Place", backref="cities",
                           cascade='all, delete, delete-orphan')
